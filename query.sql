423ec240ad88d6d417e82762a0aa8396dc8732f9
Tim Zhang <tmzpanda@gmail.com>
2021-02-11 02:03:25 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,19 +1,18 @@
---  WITH AS
+-- WITH AS
 /*
-
-
+A replacement to sub-query.
 
 
 */
 -- Find departments where average salary of employees in that department is higher than the average salary of all employees in the company.
 WITH 
-department_salary(department, average_salary) as
+department_salary(department, average_salary) AS
 (
   SELECT department, avg(salary)
   FROM salaries
   GROUP BY department
 ),
-company_salary(average_salary) as 
+company_salary(average_salary) AS
 (
   SELECT avg(salary)
   FROM salaries 
@@ -21,3 +20,21 @@ company_salary(average_salary) as
 SELECT department
 FROM department_salary, company_salary
 WHERE department_salary.average_salary > company_salary.average_salary;
+
+
+
+
+
+-- Function
+-- Nth Highest Salary
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    DECLARE K INT;
+    SET K = N - 1;
+    RETURN (       
+        SELECT DISTINCT Salary
+        FROM Employee
+        ORDER BY Salary DESC 
+        LIMIT 1 OFFSET K
+    );
+END
