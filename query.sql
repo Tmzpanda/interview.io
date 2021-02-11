41abddbb188af9f79a37d5f1dc7de7684b691f42
Tim Zhang <tmzpanda@gmail.com>
2021-02-11 17:55:46 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,29 +1,4 @@
--- WITH AS
-/*
-A replacement to sub-query.
-
-
-*/
--- Departments where the average salary of this department is higher than the average salary of the company.
-WITH 
-department_salary(department, average_salary) AS
-(
-  SELECT department, avg(salary)
-  FROM salaries
-  GROUP BY department
-),
-company_salary(average_salary) AS
-(
-  SELECT avg(salary)
-  FROM salaries 
-)
-SELECT department
-FROM department_salary, company_salary
-WHERE department_salary.average_salary > company_salary.average_salary;
-
-
-
--- Second Highest Salary
+-- 176. Second Highest Salary
 WITH temp(salary) AS
 (   
     SELECT DISTINCT Salary
@@ -34,7 +9,7 @@ WITH temp(salary) AS
 SELECT IFNULL((SELECT salary AS SecondHighestSalary FROM temp), NULL) AS SecondHighestSalary
 
 
--- Nth Highest Salary
+-- 177. Nth Highest Salary
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
     DECLARE K INT;
@@ -46,27 +21,105 @@ BEGIN
         LIMIT 1 OFFSET K
     );
 END
-
 SELECT db.getNthHighestSalary(10)
 
 
 
+
 -- Window Funtion
--- Highest Salary For each Department
-
-WITH temp(department_id, max_salary) AS
+-- 184. Highest Salary For each Department
+WITH Temp(DepartmentId, MaxSalary) AS
 (
-  SELECT DepartmentId, MAX(Salary)
-  FROM Employee
-  GROUP BY DepartmentId
+    SELECT DepartmentId, MAX(Salary)
+    FROM Employee
+    GROUP BY DepartmentId
 )
-SELECT
-    Department.name AS 'Department',
-    Employee.name AS 'Employee',
-    Salary
+SELECT Department.Name AS Department, Employee.Name AS Employee, Salary 
 FROM Employee
-JOIN temp
-ON temp.DepartmentId = Department.Id
+JOIN Department 
+ON Employee.DepartmentId = Department.Id
+WHERE (Employee.DepartmentId, Salary) IN (SELECT Temp.DepartmentId, Temp.MaxSalary FROM Temp)
+
+
+-- 185. Top Three Salaries For each Department
+WITH Temp AS
+(
+    SELECT *, DENSE_RANK() OVER(PARTITION BY DepartmentId ORDER BY Salary DESC) AS 'Rank'  
+    FROM Employee  
+)
+SELECT Department.Name AS Department, Temp.Name AS Employee, Temp.Salary 
+FROM Temp
+JOIN Department 
+ON Temp.DepartmentId = Department.Id
+WHERE Temp.Rank = 1 OR Temp.Rank = 2 OR Temp.Rank = 3
+
+
+
+
+-- 615. Average Salary for each Month: Departments vs Company
+WITH 
+department_salary AS (
+    SELECT DATE_FORMAT(s.pay_date, '%Y-%m') AS pay_month, e.department_id, AVG(s.amount) AS department_average
+    FROM salary s
+    JOIN employee e
+    ON s.employee_id = e.employee_id
+    GROUP BY DATE_FORMAT(s.pay_date, '%Y-%m'), e.department_id
+), 
+company_salary AS (
+    SELECT DATE_FORMAT(s.pay_date, '%Y-%m') AS pay_month, AVG(amount) AS company_average
+    FROM salary s
+    GROUP BY DATE_FORMAT(s.pay_date, '%Y-%m')
+)
+SELECT department_salary.pay_month, department_salary.department_id, 
+    CASE 
+        WHEN department_salary.department_average > company_salary.company_average THEN 'higher'
+        WHEN department_salary.department_average < company_salary.company_average THEN 'lower'
+        ELSE 'same'
+    END AS 'comparison'
+FROM department_salary
+JOIN company_salary
+ON department_salary.pay_month = company_salary.pay_month
+
+
+-- Departments where the average salary of this department is higher than the average salary of the company.
+WITH 
+department_salary(department, average_salary) AS
+(
+  SELECT department, avg(salary)
+  FROM salaries
+  GROUP BY department
+)
+SELECT Department
+FROM department_salary
+WHERE department_salary.average_salary > (SELECT avg(salary) FROM salaries);
+
+
+
+
+-- 1555. Bank Account Summary
+WITH 
+Temp AS
+(   
+    SELECT paid_by AS user_id, -amount AS transaction
+    FROM Transaction
+    UNION
+    SELECT paid_to AS user_id, amount AS transaction
+    FROM Transaction
+),
+User_transaction AS
+(
+    SELECT user_id, SUM(transaction) AS transaction
+    FROM Temp
+    GROUP BY user_id
+)
+SELECT Users.user_id, Users.user_name, User_transaction.transaction,
+     CASE 
+          WHEN Users.credit + User_transaction.transactions > 0 THEN 'No'
+          ELSE 'Yes'
+      END AS 'credit_limit_breached'
+FROM Users
+JOIN User_transaction
+ON Users.user_id = User_transaction.user_id
 
 
 
@@ -74,9 +127,33 @@ ON temp.DepartmentId = Department.Id
 
 
 
-普通quuery
-with
-nth highest salary for eacch deparmenet
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## oa 几道题
+
+
+
+
+
+
+
 
 
 
