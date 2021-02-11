a60267a0795e3efea6f0f7816bec2465215ed9e3
Tim Zhang <tmzpanda@gmail.com>
2021-02-11 01:21:05 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,1 +1,23 @@
+--  WITH AS
+/*
 
+
+
+
+*/
+-- Find departments where average salary of employees in that department is higher than the average salary of all employees in the company.
+WITH 
+department_salary(department, average_salary) as
+(
+  SELECT department, avg(salary)
+  FROM salaries
+  GROUP BY department
+),
+company_salary(average_salary) as 
+(
+  SELECT avg(salary)
+  FROM salaries 
+)
+SELECT department
+FROM department_salary, company_salary
+WHERE department_salary.average_salary > company_salary.average_salary;
